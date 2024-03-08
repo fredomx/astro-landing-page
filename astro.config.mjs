a64983d4ecda92b3bdf4e11952747d5a3f0ab593
fredomx <alfreduoh@gmail.com>
2024-03-08 17:03:27 -0600

    CMS config added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,39 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    NetlifyCMS({
+      
+      config: {
+    
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          // Define a blog post collection
+    {
+      name: 'posts',
+      label: 'Blog Posts',
+      folder: 'src/pages/posts',
+      create: true,
+      delete: true,
+      fields: [
+        { name: 'title', widget: 'string', label: 'Post Title' },
+        { name: 'body', widget: 'markdown', label: 'Post Body' },
+      ],
+    },
+        ],
+        adminPath: '/admin',
+      },
+    }),
+  
+  ],
   vite: {
     ssr: {
       external: ["svgo"],
     },
+
   },
 });
