a2deb93cc3b4a44165ae114f8a603168d02af35e
fredomx <71293321+fredomx@users.noreply.github.com>
2024-05-29 16:04:06 -0600

    Revert "CMS config added"
    This reverts commit a64983d4ecda92b3bdf4e11952747d5a3f0ab593.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,39 +4,10 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [
-    tailwind(),
-    NetlifyCMS({
-      
-      config: {
-    
-        backend: {
-          name: 'git-gateway',
-          branch: 'main',
-        },
-        collections: [
-          // Define a blog post collection
-    {
-      name: 'posts',
-      label: 'Blog Posts',
-      folder: 'src/pages/posts',
-      create: true,
-      delete: true,
-      fields: [
-        { name: 'title', widget: 'string', label: 'Post Title' },
-        { name: 'body', widget: 'markdown', label: 'Post Body' },
-      ],
-    },
-        ],
-        adminPath: '/admin',
-      },
-    }),
-  
-  ],
+  integrations: [tailwind()],
   vite: {
     ssr: {
       external: ["svgo"],
     },
-
   },
 });
